20b3f27fc5b7132dae3af08b0ced9c81233b7f28
0ivaan0 <ivanperezminguez@gmail.com>
2024-04-02 13:49:41 +0200

    Primer commit: Subida de proyecto

---
+++ PracticaUD5entornos/src/practica/Main.java
@@ -0,0 +1,25 @@
+package practica;
+
+/**
+ * Clase principal que prueba la funcionalidad de las clases Deporte y GestorDeportes.
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Crear algunos deportes
+        Deporte futbol = new Deporte("Fútbol", 22);
+        Deporte baloncesto = new Deporte("Baloncesto", 10);
+
+        // Crear un gestor de deportes
+        GestorDeportes gestor = new GestorDeportes();
+
+        // Agregar los deportes al gestor
+        gestor.agregarDeporte(futbol);
+        gestor.agregarDeporte(baloncesto);
+
+        // Imprimir la lista de deportes
+        System.out.println("Lista de deportes:");
+        for (Deporte deporte : gestor.getDeportes()) {
+            System.out.println("Nombre: " + deporte.getNombre() + ", Jugadores: " + deporte.getCantidadJugadores());
+        }
+    }
+}
